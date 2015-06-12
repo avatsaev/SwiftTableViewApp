9fec52a7334c92d97746207c474c12942558ad44
A. Vatsaev <azero79@gmail.com>
2015-06-12 13:06:36 +0200

    Updated deployment target to iOS9 and updated syntax to Swift 2.0

--- CheckList/AppDelegate.swift
+++ CheckList/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool{
 
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         // Override point for customization after application launch.
